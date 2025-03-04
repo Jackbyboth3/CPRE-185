d292dfd30045a6fa4a3d5155004f2a4eab3ccc96
itsyourboifroggy <jackson.byboth@gmail.com>
2025-03-03 20:48:29 -0600

    added lab5 updates

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,42 +1,76 @@
 #include <stdio.h>
 #include <math.h>
 
-double a,b,c;
+// Global variables to store acceleration data
+double a, b, c;
+
+// Function prototypes
 double mag(double x, double y, double z);
-int close_to (double tolerance, double point, double value);
+int closeTo(double tolerance, double point, double value);
 
-int main () {
-    printf(" %s", "Jack Byboth\n");
-    printf(" %s", "Jackbyb\n");
+int main() {
+    printf("%s", "Jack Byboth\n");
+    printf("%s", "Jackbyb\n");
 
-    scanf("%lf, %lf, %lf", a , b, c);
-    print(" %s","Ok, im now receiving data");
+    printf("%s", "Ok, I'm now receiving data\n");
     
-    // waiting 
-    //while(closeTo(mag())) {
-        //printf(" %s ""im Waiting");
-        // timer printf(" %s" ".");
-    //}
-
-    // falling 
-    //while (closeTo(mag())) {
-        //print("")
-    //}
+    // Waiting phase
+    int waitCounter = 0;
+    while (1) {
+        scanf("%lf, %lf, %lf", &a, &b, &c);
+        double acceleration = mag(a, b, c);
+        
+        if (closeTo(0.5, 9.8, acceleration)) { // Assuming 9.8 m/s^2 as normal gravity
+            waitCounter++;
+            if (waitCounter % 10 == 0) {
+                printf(".");
+                fflush(stdout);
+            }
+        } else {
+            printf("\nHelp me! I'm falling!!!!!!!!!!!!!\n");
+            break;
+        }
+    }
     
-    // Ouch 
-    // printf(" %s, metersVar, secondsVar ","Ouch! I fell {meters} in {seconds}")
+    // Falling phase
+    double startTime = 0, endTime = 0;
+    int fallCounter = 0;
+    while (1) {
+        scanf("%lf, %lf, %lf", &a, &b, &c);
+        double acceleration = mag(a, b, c);
+        
+        if (fallCounter == 0) {
+            startTime = clock(); // Start timing the fall
+        }
+        
+        fallCounter++;
+        if (fallCounter % 5 == 0) {
+            printf("!");
+            fflush(stdout);
+        }
+        
+        if (closeTo(2.0, 9.8, acceleration)) { // Detect landing
+            endTime = clock();
+            break;
+        }
+    }
+    
+    // Compute time and fall distance
+    double fallTime = (endTime - startTime) / CLOCKS_PER_SEC;
+    double distance = 0.5 * 9.8 * fallTime * fallTime;
+    
+    // Print final output
+    printf("\nOuch! I fell %.3f meters in %.3f seconds.\n", distance, fallTime);
+    
     return 0;
 }
 
+// Function to compute the magnitude of acceleration
 double mag(double x, double y, double z) {
-    return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-}
-//need closeTo function
-int closeTo(double tolerance, double point, double value){
-
+    return sqrt(x * x + y * y + z * z);
 }
 
-
-
-
-
+// Function to check if a value is close to a reference point within a given tolerance
+int closeTo(double tolerance, double point, double value) {
+    return fabs(value - point) < tolerance;
+}
